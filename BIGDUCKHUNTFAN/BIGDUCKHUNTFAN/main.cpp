51ea5c54a22d8df74ea79c633282fba80209af21
Daniel West <daniel.west@mds.ac.nz>
2023-10-24 16:34:57 +1300

    idk

--- BIGDUCKHUNTFAN/BIGDUCKHUNTFAN/main.cpp
+++ BIGDUCKHUNTFAN/BIGDUCKHUNTFAN/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "include/SFML/Graphics.hpp"
+
 int main()
 {
 	std::cout << ":3 uwu" << std::endl;
+
+	sf::RenderWindow Debug(sf::VideoMode(400, 400), "Debug Window");
+
 	return 0;
 }
