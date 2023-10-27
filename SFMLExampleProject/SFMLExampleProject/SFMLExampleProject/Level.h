b60c1aa990490cbbe40a2413fe81ecd420633807
Nicholas-Stokes-MDS <nicholas.stokes@mds.ac.nz>
2023-10-28 11:35:46 +1300

    new classes

--- SFMLExampleProject/SFMLExampleProject/SFMLExampleProject/Level.h
+++ SFMLExampleProject/SFMLExampleProject/SFMLExampleProject/Level.h
@@ -1,5 +1,40 @@
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+//
+// (c) Media Design School
+//
+// File Name : Level.h
+// Description : Class responsible for generating the level based on the supplied text file
+// Author : Nick Stokes
+// Mail : Nicholas.Stokes@mds.ac.nz
 #pragma once
+#include "SFML/Graphics.hpp"
+#include <fstream>
 class Level
 {
+public:
+	int iCurrentLevel = 1;
+
+	static const int levelWidth = 30;
+	static const int levelHeight = 30;
+
+	// functional
+	//std::vector<Character*> m_LevelTiles; // level
+
+	/* - Add Blow/fan pickup here - */
+
+	// load file
+	char cLevelArray[levelWidth][levelHeight];
+
+	Level();
+	~Level();
+
+	void LoadLevel(std::string _sFilePath);
+	void CreateLevel();
+	void Draw(sf::RenderWindow* _Window);
+
+	void UnloadLevel();
 };
 
