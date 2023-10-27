1c0a95f66e610d2a02ea371e4c2e01585dda168b
Nicholas-Stokes-MDS <nicholas.stokes@mds.ac.nz>
2023-10-28 12:20:15 +1300

    gaminh

--- SFMLExampleProject/SFMLExampleProject/SFMLExampleProject/main.cpp
+++ SFMLExampleProject/SFMLExampleProject/SFMLExampleProject/main.cpp
@@ -5,11 +5,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-
-    Object Player(ObjectType::Type_Player, sf::Vector2f(400,300));
-    Object Trap(ObjectType::Type_Trap, sf::Vector2f(500, 300));
-
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Game!");
     window.setFramerateLimit(60);
 
     Troop* pSoldier = new Troop("Troops/Soldier.txt");
@@ -31,17 +27,7 @@ int main()
             pSoldier->PlaceTroop(event, &window);
         }
 
-        Player.Input();
-
-        if (Player.m_ObjShape.getGlobalBounds().intersects(Trap.m_ObjShape.getGlobalBounds()))
-        {
-            window.close();
-        }
-
         window.clear();
-
-        window.draw(Player.m_ObjShape);
-        window.draw(Trap.m_ObjShape);
 
         window.display();
     }
