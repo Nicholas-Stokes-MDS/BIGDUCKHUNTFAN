3ad2478e688afd8441bb2b5eb0c32751390984b4
Nicholas-Stokes-MDS <nicholas.stokes@mds.ac.nz>
2023-11-18 19:10:27 +1300

    .

--- SFMLExampleProject/SFMLExampleProject/SFMLExampleProject/LevelManager.h
+++ SFMLExampleProject/SFMLExampleProject/SFMLExampleProject/LevelManager.h
@@ -22,6 +22,9 @@ public:
 	int g_iLevel1Troops[6] = { 1, 1, 1, 1, 1, 1 };
 	int g_iLevel2Troops[6] = { 2, 0, 1, 1, 3, 0 };
 	int g_iLevel3Troops[6] = { 3, 3, 1, 0, 0, 1 };
+	int g_iLevel4Troops[6] = { 1, 2, 3, 4, 5, 6 };
+	int g_iLevel5Troops[6] = { 6, 5, 4, 3, 2, 1 };
+	int g_iLevel6Troops[6] = { 1, 3, 5, 3, 1, 0 };
 
 	LevelManager(const LevelManager& _Manager) = delete; // when try to copy, deletes it only should be one (stops copy constructor from working)
 
